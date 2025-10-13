9428381a9df4ac211c4653e8e0df24c12c197449
Vitor-Calliari <vitorcalliari@hotmail.com>
2025-10-13 19:13:33 -0300

    feat: implementada tela de login simples e implementação do token de autenticação de login sem banco por enquanto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'views/pages/home_page.dart';
+import 'views/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(),
+      home: const LoginPage(),
     );
   }
 }
