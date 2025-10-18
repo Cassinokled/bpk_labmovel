18f67b805d3c141f7bf11cc306915d7a075a3d3b
NevesRuan <neves.ruan23@gmail.com>
2025-10-18 17:25:53 -0300

    chore: setup firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'views/pages/home_page.dart';
 import 'views/pages/login.dart';
 import 'providers/carrinho_emprestimo_provider.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
   runApp(const MyApp());
 }
 
